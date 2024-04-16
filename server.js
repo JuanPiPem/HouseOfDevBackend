aadccafbadd9d32813212051868eaf68f2bc8c85
JuanPiPem <jplezcano75@gmail.com>
2024-04-15 22:20:15 -0700

    add db.config and modified server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
+const db = require("./api/config/db.config");
 
 app.use(express.json());
 app.use(logger("dev"));
 
-app.listen(8080, () => {
-  console.log("Server listening in port 8080");
+db.sync({ force: false }).then(() => {
+  app.listen(8080, () => console.log(`Servidor en el puerto 8080`));
 });
+module.exports = app;
